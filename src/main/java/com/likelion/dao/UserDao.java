711951db8baf33e103f948fa58afdb89e06cf63b
gemdoq <gemdoq@gmail.com>
2022-10-25 11:27:28 +0900

    separation of interests(method)

--- src/main/java/com/likelion/dao/UserDao.java
+++ src/main/java/com/likelion/dao/UserDao.java
@@ -9,14 +9,7 @@ import static java.lang.Class.forName;
 
 public class UserDao {
 
-    /*
-    환경변수에서 값을 읽어와서 맵에 담고 각 변수를 선언하여 맵에 담은 자료에서 키에 해당하는 값들을 각각 담아준다
-    클래스 이름으로 드라이버를 가져오는 코드(class.forname(드라이브명))
-    Connection타입 변수에 드라이브매니저클래스의 getconnection(개인정보)메서드로 연결을 담기
-    PreparedStatement타입 변수에 접속한 객체 내의 prepareStatement()메서드로 쿼리문을 담기
-    해당되는 PreparedStatement타입 객체의 키에 해당하는 문자열 값을 변경한다.
-     */
-    public void add(User user) throws ClassNotFoundException, SQLException {
+    private Connection makeConnection() throws ClassNotFoundException, SQLException {
         Map<String, String> env = System.getenv();
         String dbHost = env.get("DB_HOST");
         String dbUser = env.get("DB_USER");
@@ -25,6 +18,11 @@ public class UserDao {
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection c = DriverManager.getConnection(dbHost, dbUser, dbPassword);
 
+        return c;
+    }
+
+    public void add(User user) throws ClassNotFoundException, SQLException {
+        Connection c = makeConnection();
         PreparedStatement ps = c.prepareStatement("insert into users(id,name,password) values(?,?,?)");
         ps.setString(1, user.getId());
         ps.setString(2, user.getName());
@@ -35,20 +33,9 @@ public class UserDao {
         ps.close();
         c.close();
     }
-    /*
-    add()처럼 findById()도 구현
-    연결은 똑같고, 쿼리문의 내용이 select문으로 변경
-    ResultSet은 select문의 결과를 받는 객체,
-     */
+
     public void findById(String id) throws ClassNotFoundException, SQLException {
-        Map<String, String> env = System.getenv();
-        String dbHost = env.get("DB_HOST");
-        String dbUser = env.get("DB_USER");
-        String dbPassword = env.get("DB_PASSWORD");
-
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection c = DriverManager.getConnection(dbHost, dbUser, dbPassword);
-
+        Connection c = makeConnection();
         PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
         ps.setString(1, id);
         System.out.println(ps);//쿼리문을 출력
