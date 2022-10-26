4994a9b31b5685e4982d46c6cd9da38e57558f50
gemdoq <gemdoq@gmail.com>
2022-10-26 10:00:31 +0900

    applied jdbcTemplate to both get() and getAll() + TEST method getAll()

--- src/main/java/com/likelion/dao/UserDao.java
+++ src/main/java/com/likelion/dao/UserDao.java
@@ -2,9 +2,11 @@ package com.likelion.dao;
 
 import com.likelion.domain.User;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.sql.*;
+import java.util.List;
 
 public class UserDao {
 
@@ -29,8 +31,8 @@ public class UserDao {
 //        });
 
 //        jdbcContext.excuteSql("delete from users");
-
-        jdbcTemplate.update("delete from users");
+        String sql = "delete from users";
+        jdbcTemplate.update(sql);
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
@@ -45,78 +47,93 @@ public class UserDao {
 //                return ps;
 //            }
 //        });
-
-        jdbcTemplate.update("insert into users(id,name,password) values(?,?,?);", user.getId(), user.getName(), user.getPassword());
-    }
-
-    public User findById(String id) throws ClassNotFoundException, SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        User user = null;
-
-        try {
-            c = dataSource.getConnection();
-            ps = c.prepareStatement("select * from users where id = ?");
-            ps.setString(1, id);
-            System.out.println(ps);//쿼리문을 출력
-            rs = ps.executeQuery();//ResultSet이 쿼리실행문을 담아오는 객체
-
-            if (rs.next()) {
-                System.out.println(rs.getString(2));
-                System.out.println(rs.getString(3));
-                user = new User(rs.getString(1), rs.getString(2), rs.getString(3));
-            }
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            //사용한 객체를 닫을 때는 역순으로
-            if(rs!=null) try {
-                rs.close();
-            } catch (SQLException ignore) {
-            }
-            if(ps!=null) try {
-                ps.close();
-            } catch (SQLException ignore) {
-            }
-            if(c!=null) try {
-                c.close();
-            } catch (SQLException ignore) {
-            }
-        }
-        return user;
+        String sql = "insert into users(id,name,password) values(?,?,?);";
+        jdbcTemplate.update(sql, user.getId(), user.getName(), user.getPassword());
     }
 
     public int getCount() throws SQLException, ClassNotFoundException {
-        Connection c = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-        int count = 0;
+//        Connection c = null;
+//        PreparedStatement ps = null;
+//        ResultSet rs = null;
+//        int count = 0;
+//
+//        try {
+//            c = dataSource.getConnection();
+//            ps = c.prepareStatement("select count(*) from users");
+//            rs = ps.executeQuery();
+//
+//            if(rs.next()) {
+//                count = rs.getInt(1);
+//            }
+//        } catch (SQLException e) {
+//            throw e;
+//        } finally {
+//            if(rs!=null) try {
+//                rs.close();
+//            } catch (SQLException ignore) {
+//            }
+//            if(ps!=null) try {
+//                ps.close();
+//            } catch (SQLException ignore) {
+//            }
+//            if(c!=null) try {
+//                c.close();
+//            } catch (SQLException ignore) {
+//            }
+//        }
+//        return count;
+        String sql = "select count(*) from users";
+        return jdbcTemplate.queryForObject(sql, Integer.class);
+    }
 
-        try {
-            c = dataSource.getConnection();
-            ps = c.prepareStatement("select count(*) from users");
-            rs = ps.executeQuery();
-
-            if(rs.next()) {
-                count = rs.getInt(1);
-            }
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if(rs!=null) try {
-                rs.close();
-            } catch (SQLException ignore) {
-            }
-            if(ps!=null) try {
-                ps.close();
-            } catch (SQLException ignore) {
-            }
-            if(c!=null) try {
-                c.close();
-            } catch (SQLException ignore) {
-            }
+    RowMapper<User> rowMapper = new RowMapper<User>() {
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new User(rs.getString("id"), rs.getString("name"), rs.getString("password"));
         }
-        return count;
+    };
+    public User findById(String id) throws ClassNotFoundException, SQLException {
+//        Connection c = null;
+//        PreparedStatement ps = null;
+//        ResultSet rs = null;
+//        User user = null;
+//
+//        try {
+//            c = dataSource.getConnection();
+//            ps = c.prepareStatement("select * from users where id = ?");
+//            ps.setString(1, id);
+//            System.out.println(ps);//쿼리문을 출력
+//            rs = ps.executeQuery();//ResultSet이 쿼리실행문을 담아오는 객체
+//
+//            if (rs.next()) {
+//                System.out.println(rs.getString(2));
+//                System.out.println(rs.getString(3));
+//                user = new User(rs.getString(1), rs.getString(2), rs.getString(3));
+//            }
+//        } catch (SQLException e) {
+//            throw e;
+//        } finally {
+//            //사용한 객체를 닫을 때는 역순으로
+//            if(rs!=null) try {
+//                rs.close();
+//            } catch (SQLException ignore) {
+//            }
+//            if(ps!=null) try {
+//                ps.close();
+//            } catch (SQLException ignore) {
+//            }
+//            if(c!=null) try {
+//                c.close();
+//            } catch (SQLException ignore) {
+//            }
+//        }
+//        return user;
+        String sql = "select * from users where id = ?";
+        return jdbcTemplate.queryForObject(sql, rowMapper, id);
+    }
+
+    public List<User> getAll() {
+        String sql = "select * from users order by id";
+        return jdbcTemplate.query(sql, rowMapper);
     }
 }
--- src/test/java/com/likelion/dao/UserDaoTest.java
+++ src/test/java/com/likelion/dao/UserDaoTest.java
@@ -13,6 +13,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -59,5 +60,20 @@ class UserDaoTest {
         assertEquals(3,userDao.getCount());
 
         userDao.deleteAll();
+    }
+
+    @Test
+    void getAll() throws SQLException, ClassNotFoundException {
+        userDao.deleteAll();
+
+        List<User> users = userDao.getAll();
+        assertEquals(0, users.size());
+
+
+        userDao.add(user1);
+        userDao.add(user2);
+        userDao.add(user3);
+        users = userDao.getAll();
+        assertEquals(3, users.size());
     }
 }
